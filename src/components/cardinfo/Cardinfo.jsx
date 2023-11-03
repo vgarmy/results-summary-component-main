43d36ab3ca4dc737cc1572e16c52dd4c3fe5df6e
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-03 21:30:46 +0100

    Small css fixes

--- src/components/cardinfo/Cardinfo.jsx
+++ src/components/cardinfo/Cardinfo.jsx
@@ -5,16 +5,18 @@ import data from './result.json';
 
 const listItems = data.map(data =>
     <div className={'summary_card ' + data.category}>
-          <span><img src={data.image} alt={data.category} />{data.category}</span><span><span className='summary_card_dark'>{data.score}<span className='summary_card_light'> / 100</span></span></span>
-       </div>
-  );
+        <span><img src={data.image} alt={data.category} />{data.category}</span>
+        <span><span className='summary_card_dark'>{data.score}<span className='summary_card_light'> / 100</span>
+        </span></span>
+    </div>
+);
 
-  const Cardinfo = () => {
-      return(
-          <div className='summary-container'>
-              { listItems }
-          </div>
-        )
-  }
+const Cardinfo = () => {
+    return (
+        <div className='summary-container'>
+            {listItems}
+        </div>
+    )
+}
 
-  export default Cardinfo
+export default Cardinfo
