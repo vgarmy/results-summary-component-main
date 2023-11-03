c62d9ed02e8ae406fe7b7812b636e9272fe771fa
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-03 21:47:05 +0100

    card fixes

--- src/components/cardinfo/Cardinfo.jsx
+++ src/components/cardinfo/Cardinfo.jsx
@@ -5,7 +5,7 @@ import data from './result.json';
 
 const listItems = data.map(data =>
     <div className={'summary_card ' + data.category}>
-        <span><img src={data.image} alt={data.category} />{data.category}</span>
+        <span className='summary-d-flex'><img src={data.image} alt={data.category} />{data.category}</span>
         <span><span className='summary_card_dark'>{data.score}<span className='summary_card_light'> / 100</span>
         </span></span>
     </div>
