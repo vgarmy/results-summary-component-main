80c5fdc0e1bc38d18de2016d168252e3a21e36e5
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-06 10:42:42 +0100

    Landmark

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div className='App'>
-        <div className='container'>
+        <div className='container' role="main">
             <Card />
             <Footer />
         </div>
